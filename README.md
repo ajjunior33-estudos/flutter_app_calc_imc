6eca4bb82d4b34f92ddeec18d83b95289395e51e
André Souza <ajjunior33@gmail.com>
2022-12-11 03:40:12 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculadora IMC Flutter
+
+# Tasks
---
+++ android/app/src/main/kotlin/br/dev/meraki/imc/calc/imc_calc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.dev.meraki.imc.calc.imc_calc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
